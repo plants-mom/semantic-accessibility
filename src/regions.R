2c8b2866527872890bb898133a8d8e358bedf9b2
jas <jaswinko@protonmail.com>
2021-11-16 16:47:51 +0100

    remove old coding

--- src/regions.R
+++ src/regions.R
@@ -23,17 +23,9 @@ dataf <- read_csv(here("results/allACTFiles_vp_clean.csv")) %>%
       quan_cond == "GEEN" & subj_cond == "MIS" & obj_cond == "MATCH" ~ "g",
       quan_cond == "GEEN" & subj_cond == "MIS" & obj_cond == "MIS" ~ "h"
     ),
-    quants = ifelse(quan_cond == "EEN", 0.5, -0.5),
-    typic = ifelse(cond %in% c("a", "b", "e", "f"), 0.5, -0.5),
-    interf = ifelse(cond %in% c("a", "c", "e", "g"), 0.5, -0.5),
-    quant_x_typic = ifelse(cond %in% c("a", "b", "g", "h"), 0.5, -0.5),
-    quant_x_inter = ifelse(cond %in% c("a", "c", "f", "h"), 0.5, -0.5),
-    quant_x_interf_TYP = ifelse(cond %in% c("a", "f"), 0.5,
-      ifelse(cond %in% c("b", "e"), -0.5, 0)
-    ),
-    quant_x_interf_ATY = ifelse(cond %in% c("c", "h"), 0.5,
-      ifelse(cond %in% c("d", "g"), -0.5, 0)
-    ),
+    quants = ifelse(quan_cond == "EEN", -1, 1),
+    typic = ifelse(cond %in% c("a", "b", "e", "f"), -1, 1),
+    interf = ifelse(cond %in% c("a", "c", "e", "g"), 1, -1),
     ## additional variables
     rrdur = totfixdur - (gdur - gsacc), # re-reading duration
     rr = rrdur > 0
@@ -43,12 +35,6 @@ dataf <- read_csv(here("results/allACTFiles_vp_clean.csv")) %>%
 vars <- c("subj", "item", "rpdur", "tgdur", "totfixdur", "gbck", "gdur", "rr", "rrdur")
 
 dataf %>%
-  # simpler models
-  mutate(
-    quants = ifelse(quan_cond == "EEN", -1, 1),
-    typic = ifelse(cond %in% c("a", "b", "e", "f"), -1, 1),
-    interf = ifelse(cond %in% c("a", "c", "e", "g"), 1, -1)
-  ) %>%
   filter(ffdur != 0) %>%
   select(
     region,
