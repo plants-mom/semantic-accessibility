4b0b967b9eadf7eaaa18587e3cecaa1aa5b0d08f
jas <jaswinko@protonmail.com>
2021-11-25 13:05:43 +0100

    wrap everything in functions

--- src/plots.R
+++ src/plots.R
@@ -9,59 +9,90 @@ library(knitr)
 library(dplyr)
 library(ggplot2)
 library(cowplot)
+library(readr)
+library(tidyr)
+library(purrr)
 library(ggtext)
 
 ##
 ## prior predictive check plots
 ##
 
-source(here("src/priors.R"))
-sim_summary <- read.csv(here("results/sim_summary_bin.csv"))
+ppc_rt_plots <- function() {
+  source(here("src/priors.R"))
+  sim_summary <- read.csv(here("results/sim_summary_bin.csv"))
 
 
 
-capt <- priors %>%
-  select(class, prior) %>%
-  kable(format = "simple")
+  capt <- priors %>%
+    select(class, prior) %>%
+    kable(format = "simple")
 
-ccc <- with(priors, paste(class, prior, collapse = "<br>"))
+  ccc <- with(priors, paste(class, prior, collapse = "<br>"))
 
-scsum_plot <- sim_summary %>%
-  filter(stat != "ieff_rt") %>%
-  ggplot(aes(rt)) +
-  scale_x_continuous("Reaction times in ms",
-    trans = "log",
-    breaks = c(0.001, 1, 100, 1000, 10000, 10000000),
-    labels = function(n) {
-      format(n, scientific = 5, digits = 3)
-    }
-  ) +
-  geom_histogram(bins = 50) +
-  facet_wrap(~stat, ncol = 1) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(caption = ccc) +
-  theme(
-    plot.caption.position = "plot",
-    plot.caption = element_markdown(
-      size = 11,
-      ## lineheight = 1,
-      padding = margin(5.5, 5.5, 5.5, 5.5),
-      margin = margin(0, 0, 5.5, 0),
-      ## linetype = 1
+  scsum_plot <- sim_summary %>%
+    filter(stat != "ieff_rt") %>%
+    ggplot(aes(rt)) +
+    scale_x_continuous("Reaction times in ms",
+      trans = "log",
+      breaks = c(0.001, 1, 100, 1000, 10000, 10000000),
+      labels = function(n) {
+        format(n, scientific = 5, digits = 3)
+      }
+    ) +
+    geom_histogram(bins = 50) +
+    facet_wrap(~stat, ncol = 1) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    labs(caption = ccc) +
+    theme(
+      plot.caption.position = "plot",
+      plot.caption = element_markdown(
+        size = 11,
+        ## lineheight = 1,
+        padding = margin(5.5, 5.5, 5.5, 5.5),
+        margin = margin(0, 0, 5.5, 0),
+        ## linetype = 1
+      )
     )
-  )
 
 
-inter <- int_eff <- sim_summary %>%
-  filter(stat == "ieff_rt") %>%
-  mutate(
-    rt = replace(rt, rt >= 10000, 10000),
-    rt = replace(rt, rt <= -10000, -10000)
-  ) %>%
-  ggplot(aes(rt)) +
-  geom_histogram(bins = 100)
+  inter <- int_eff <- sim_summary %>%
+    filter(stat == "ieff_rt") %>%
+    mutate(
+      rt = replace(rt, rt >= 10000, 10000),
+      rt = replace(rt, rt <= -10000, -10000)
+    ) %>%
+    ggplot(aes(rt)) +
+    geom_histogram(bins = 100)
 
-pp_checks <- plot_grid(scsum_plot, inter)
-pp_checks
+  pp_checks <- plot_grid(scsum_plot, inter)
+  pp_checks
 
-ggsave(here("figs/prior_pred_6.png"), pp_checks, width = 16, height = 8)
+  ggsave(here("figs/prior_pred_6.png"), pp_checks, width = 16, height = 8)
+}
+
+ppc_params_plots <- function() {
+
+  ## ppc prior sample binomial
+  ##
+
+  simdata <- read_csv(here("results/ppc_params_sample_binom.csv"))
+
+  simdata %>%
+    modify(rethinking::inv_logit) %>%
+    pivot_longer(cols = everything(), names_to = "parameter") %>%
+    ggplot(., aes(value)) +
+    geom_density() +
+    facet_wrap(~parameter)
+
+  ## plot(simd$true_params$sigma_u1)
+  ## glimpse(inv_params)
+  ## rethinking::dens(inter)
+}
+
+if (sys.nframe() == 0) {
+
+}
+
+
+ppc_params_plots()
--- src/simulations.R
+++ src/simulations.R
@@ -7,39 +7,12 @@ here::i_am("src/simulations.R")
 library(here)
 library(dplyr)
 library(tidyr)
+library(readr)
 library(designr)
 
 source(here("src/sim_functions.R"))
 
-set.seed(123)
-
-expdesign <- fixed.factor("quan_cond", levels = c("EEN", "GEEN")) +
-  fixed.factor("subj_cond", levels = c("MATCH", "MIS")) +
-  fixed.factor("obj_cond", levels = c("MATCH", "MIS")) +
-  random.factor("subj", instances = 48) +
-  random.factor("item", instances = 32)
-
-simdata <- design.codes(expdesign) %>%
-  mutate(
-    cond = case_when(
-      quan_cond == "EEN" & subj_cond == "MATCH" & obj_cond == "MATCH" ~ "a",
-      quan_cond == "EEN" & subj_cond == "MATCH" & obj_cond == "MIS" ~ "b",
-      quan_cond == "EEN" & subj_cond == "MIS" & obj_cond == "MATCH" ~ "c",
-      quan_cond == "EEN" & subj_cond == "MIS" & obj_cond == "MIS" ~ "d",
-      quan_cond == "GEEN" & subj_cond == "MATCH" & obj_cond == "MATCH" ~ "e",
-      quan_cond == "GEEN" & subj_cond == "MATCH" & obj_cond == "MIS" ~ "f",
-      quan_cond == "GEEN" & subj_cond == "MIS" & obj_cond == "MATCH" ~ "g",
-      quan_cond == "GEEN" & subj_cond == "MIS" & obj_cond == "MIS" ~ "h"
-    ),
-    quants = ifelse(quan_cond == "EEN", -1, 1),
-    typic = ifelse(cond %in% c("a", "b", "e", "f"), 1, -1),
-    interf = ifelse(cond %in% c("a", "c", "e", "g"), 1, -1),
-  )
-
-
-
-
-pp_lognorm <- function(.number_sim = number_sim,
+ppc_lognorm <- function(.number_sim = number_sim,
                        .simdata = simdata, write = TRUE) {
   frm <- "~ quants * typic * interf +
  (quants * typic * interf | subj) + (quants * typic * interf | item)"
@@ -79,4 +52,49 @@ pp_lognorm <- function(.number_sim = number_sim,
       row.names = FALSE
     )
   }
+}
+
+ppc_binom <- function(.number_sim = number_sim, .simdata = simdata) {
+  ## prior predictive checks for the binomial models
+
+  frm <- "~ quants * typic * interf +
+ (quants * typic * interf | subj) + (quants * typic * interf | item)"
+  simd <- msimulate(
+    .number_sim, .simdata, priors_binom, 8, 8, 8,
+    "binomial", frm, intercept_above_zero = FALSE, estimates = FALSE
+  )
+
+  write_csv(simd$true_params, here("results/ppc_params_sample_binom.csv"))
+  ## return(simd)
+}
+
+if (sys.nframe() == 0) {
+  number_sim <- 1e4
+  set.seed(123)
+
+  expdesign <- fixed.factor("quan_cond", levels = c("EEN", "GEEN")) +
+    fixed.factor("subj_cond", levels = c("MATCH", "MIS")) +
+    fixed.factor("obj_cond", levels = c("MATCH", "MIS")) +
+    random.factor("subj", instances = 48) +
+    random.factor("item", instances = 32)
+
+  simdata <- design.codes(expdesign) %>%
+    mutate(
+      cond = case_when(
+        quan_cond == "EEN" & subj_cond == "MATCH" & obj_cond == "MATCH" ~ "a",
+        quan_cond == "EEN" & subj_cond == "MATCH" & obj_cond == "MIS" ~ "b",
+        quan_cond == "EEN" & subj_cond == "MIS" & obj_cond == "MATCH" ~ "c",
+        quan_cond == "EEN" & subj_cond == "MIS" & obj_cond == "MIS" ~ "d",
+        quan_cond == "GEEN" & subj_cond == "MATCH" & obj_cond == "MATCH" ~ "e",
+        quan_cond == "GEEN" & subj_cond == "MATCH" & obj_cond == "MIS" ~ "f",
+        quan_cond == "GEEN" & subj_cond == "MIS" & obj_cond == "MATCH" ~ "g",
+        quan_cond == "GEEN" & subj_cond == "MIS" & obj_cond == "MIS" ~ "h"
+      ),
+      quants = ifelse(quan_cond == "EEN", -1, 1),
+      typic = ifelse(cond %in% c("a", "b", "e", "f"), 1, -1),
+      interf = ifelse(cond %in% c("a", "c", "e", "g"), 1, -1),
+    )
+
+
+  ppc_binom()
 }
