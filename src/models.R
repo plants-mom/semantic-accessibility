0daca07cfcbc8e582542ae1ca390432f2c48a3ca
jas <jaswinko@protonmail.com>
2021-12-17 16:58:37 +0100

    WIP: refactoring model fitting functions

--- src/models.R
+++ src/models.R
@@ -14,6 +14,138 @@ library(purrr)
 options(mc.cores = parallel::detectCores())
 
 
+fit_full <- function(data_list, dv_name, .priors, remove_zeros = TRUE,
+                     .family = NULL,
+                     optimize_mem = FALSE,
+                     unique_name = FALSE) {
+  frm <- formula(~ 1 + typic * interf * quants +
+    (1 + typic * interf * quants | item) +
+    (1 + typic * interf * quants | subj)) %>%
+    update.formula(paste0(dv_name, "~ . "))
+
+  if (is.null(.family)) {
+    .family <- ifelse(dv_name %in% c("gbck", "rr"), "bernoulli", "lognormal")
+  }
+
+  nms <- data_list %>%
+    map(~ select(., region)) %>%
+    map_dfr(unique) %>%
+    pull()
+
+  ## message(dv_name, typeof(dv_name))
+  ## message(str(data_list))
+
+  if (remove_zeros == TRUE) {
+    sel_data <- map(
+      data_list,
+      ~ select(., region:item, quan_cond:last_col(), {{ dv_name }})
+    ) %>%
+      map(.x = ., ~ filter(., .data[[dv_name]] > 0)) %>%
+      set_names(paste0("region_", nms))
+  } else {
+    sel_data <- map(
+      data_list,
+      ~ select(., region:item, quan_cond:last_col(), {{ dv_name }})
+    ) %>% set_names(paste0("region_", nms))
+  }
+
+  ## return(sel_data)
+  ##
+  if (unique_name == TRUE) {
+    mname <- paste(format(Sys.time(), "%s"), dv_name, "r", sep = "_")
+  } else {
+    mname <- paste(dv_name, "r", sep = "_")
+  }
+
+  full_ms <- sel_data %>%
+    map(~ brm(frm,
+      family = .family,
+      prior = .priors,
+      iter = 4000, data = .x,
+      file = here("models", paste0(mname, .x$region[1]))
+    ))
+
+
+  if (optimize_mem == TRUE) {
+    rm(full_ms, sel_data, nms)
+    gc()
+  } else {
+    rm(sel_data, nms)
+    return(full_ms)
+  }
+}
+
+
+fit_split <- function(data_list, dv_name, split_by, .priors = priors,
+                      remove_zeros = TRUE, .family = NULL, optimize_mem = FALSE,
+                      unique_name = FALSE,
+                      ) {
+
+  frm <- formula(~ 1 + typic * interf +
+    (1 + typic * interf | item) +
+    (1 + typic * interf | subj)) %>%
+    update.formula(paste0(dv_name, "~ . "))
+
+  if (is.null(.family)) {
+    .family <- ifelse(dv_name %in% c("gbck", "rr"), "bernoulli", "lognormal")
+  }
+
+  nms <- data_list %>%
+    map(~ select(., region)) %>%
+    map_dfr(unique) %>%
+    pull()
+
+  ## message(dv_name, typeof(dv_name))
+  ## message(str(data_list))
+
+  if (remove_zeros == TRUE) {
+    sel_data <- map(
+      data_list,
+      ~ select(., region:item, quan_cond:last_col(), {{ dv_name }})
+    ) %>%
+      map(.x = ., ~ filter(., .data[[dv_name]] > 0)) %>%
+      set_names(paste0("region_", nms))
+  } else {
+    sel_data <- map(
+      data_list,
+      ~ select(., region:item, quan_cond:last_col(), {{ dv_name }})
+    ) %>% set_names(paste0("region_", nms))
+  }
+
+  ## return(sel_data)
+  ##
+  if (unique_name == TRUE) {
+    mname <- paste(format(Sys.time(), "%s"), dv_name, "r", sep = "_")
+  } else {
+    mname <- paste(dv_name, "r", sep = "_")
+  }
+
+
+  sel_data <- map(sel_data, ~ select(., -quants))
+
+  split_ms <- map(sel_data, ~ split(., .$quan_cond)) %>%
+    map(~ imap(., ~ brm(frm,
+      family = .family,
+      prior = .priors,
+      iter = 4000,
+      data = .x,
+      file = here(
+        "models",
+        paste0(
+          dv_name, "_",
+          tolower(.y), "_r", .x$region[1]
+        )
+      )
+    )))
+
+  if (optimize_mem == TRUE) {
+    rm(split_ms, sel_data, frm)
+    gc()
+  } else {
+    return(split_ms)
+  }
+}
+
 
 fit_models <- function(data_list, dv_name, .priors, remove_zeros = TRUE,
                        .return = c(
@@ -152,11 +284,15 @@ if (sys.nframe() == 0) {
   ##   remove_zeros = FALSE, optimize_mem = TRUE
   ## )
 
-  ## c("gdur", "tgdur", "rpdur") %>%
-  ##   walk(~ fit_models(dfs[6], .,
-  ##     .priors = priors_narrow_int,
-  ##     optimize_mem = TRUE, .return = "full_models", unique_name = TRUE
-  ##   ))
+  c("gdur", "tgdur", "rpdur") %>%
+
+gd <- "gbck" %>%
+    map(~ fit_full(dfs[5:6], .,
+      .priors = priors
+    ))
+
+glimpse(gd)
+
 
   c("gdur", "tgdur", "rpdur") %>%
     walk(~ fit_models(dfs[6], .,
@@ -166,10 +302,22 @@ if (sys.nframe() == 0) {
       optimize_mem = TRUE, .return = "full_models", unique_name = TRUE
     ))
 
-##   c("gdur", "tgdur", "rpdur") %>%
-##     walk(~ fit_models(dfs[6:8], .,
-##       .priors = priors_ni_big_sd_sigma,
-##       optimize_mem = TRUE, .return = "full_models", unique_name = TRUE
-##     ))
-
+  ##   c("gdur", "tgdur", "rpdur") %>%
+  ##     walk(~ fit_models(dfs[6:8], .,
+  ##       .priors = priors_ni_big_sd_sigma,
+  ##       optimize_mem = TRUE, .return = "full_models", unique_name = TRUE
+  ##     ))
 }
+
+frm <- formula(~ 1 + typic * interf +
+  (1 + typic * interf | item) +
+    (1 + typic * interf | subj))
+
+
+
+update.formula(frm, . ~ . - interf)
+frm
+
+
+%>%
+  update.formula(paste0(dv_name, "~ . "))
