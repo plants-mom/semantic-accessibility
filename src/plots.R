46061beb5e32861f602cec046a923edf8ddfc7c4
jas <jaswinko@protonmail.com>
2022-05-11 18:05:07 +0200

    add the last figure

--- src/plots.R
+++ src/plots.R
@@ -156,6 +156,32 @@ rts <- function(region) {
 }
 
 
+neg_atypical_rts <- function() {
+  regions_data <- function(region) {
+    f <- compose(readRDS, here, paste0)
+    list(
+      TFD = make_plot_data(f("models/totfixdur_geen_atypical_r",
+                             region, ".rds")),
+      RB = make_plot_data_stan(f("models/tgdur_geen_atypical_stan_region",
+                                 region, ".rds"))
+    ) %>%
+      bind_rows(.id = "measure") %>%
+      mutate(region = region)
+  }
+
+  list(
+    regions_data(6),
+    regions_data(8),
+    regions_data(9)
+  ) %>%
+    bind_rows() %>%
+    mutate(region = case_when(region == 6 ~ "critical",
+                              region == 8 ~ "post-critical",
+                              region == 9 ~ "wrap-up"),
+           region = as.factor(region)) %>%
+    measure_summary_regions()
+}
+
 regr_rr <- function(region) {
   ##
   ## regression and re-reading probability plot
@@ -192,6 +218,35 @@ measure_summary <- function(plot_data,
     scale_color_brewer(palette = pal) +
     scale_fill_brewer(palette = pal)
 }
+
+measure_summary_regions <- function(plot_data,
+                            .measure = "measure",
+                            pal = cur_palette) {
+
+  ## This should be refactored with measure_summary,
+  ## but I don't have the time
+
+  dod <- 0.5
+
+  plot_data %>%
+    ggplot(., aes(fill = .data[[.measure]], color = .data[[.measure]])) +
+    geom_linerange(aes(ymin = ll, ymax = hh, x = region),
+      position = position_dodge(dod)
+    ) +
+    geom_linerange(aes(ymin = l, ymax = h, x = region),
+      size = 1.5, show.legend = FALSE,
+      position = position_dodge(dod)
+    ) +
+    geom_point(aes(y = m, x = region),
+      size = 2, shape = 21,
+      position = position_dodge(dod)
+    ) +
+    theme_minimal() +
+    theme(axis.title.x = element_blank(), axis.title.y = element_blank()) +
+    scale_color_brewer(palette = pal) +
+    scale_fill_brewer(palette = pal)
+}
+
 
 make_plot_data <- compose(
   ~ mcmc_intervals_data(.x, prob = 0.95, prob_outer = 1, point_est = "mean"),
