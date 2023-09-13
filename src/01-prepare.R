8bbf5339368924968d7344cd2dd727f91b1c3522
jas <jaswinko@protonmail.com>
2023-09-13 13:29:50 +0200

    refactor

--- src/01-prepare.R
+++ src/01-prepare.R
@@ -3,53 +3,68 @@
 ## Save dataframe without some columns
 ##
 
-here::i_am("src/prepare.R")
+here::i_am("src/01-prepare.R")
 
 
 library(dplyr)
 library(readr)
 library(here)
 
-dataf <- read_delim(here("data/allACTFiles_vp.csv"), delim = " ") %>%
-  mutate(item = as.numeric(item),
-         subjectnr = as.numeric(subjectnr))
+prepare_data <- function(data_in) {
+  dataf <- read_delim(here(data_in), delim = " ") %>%
+    mutate(
+      item = as.numeric(item),
+      subjectnr = as.numeric(subjectnr)
+    )
 
-glimpse(dataf)
-all(sort(unique(dataf$item)) == seq(32))
-length(unique(dataf$subjectnr))
+  ## glimpse(dataf)
+  ## all(sort(unique(dataf$item)) == seq(32))
+  ## length(unique(dataf$subjectnr))
 
-out <- dataf %>%
-  filter(!(item %in% c(14, 24)),
-         subjectnr != 4,
-         !(subjectnr == 40 & item == 19),
-         !(subjectnr == 13 & item == 25),
-         !(subjectnr == 43 & (item == 3 | item == 6 | item == 7)),
-         !(subjectnr == 20 & item == 30)) %>%
-  group_by(cond) %>%
-  mutate(
-    quan = strsplit(cond[1], "_")[[1]][1],
-    subj = strsplit(cond[1], "_")[[1]][2],
-    obj = strsplit(cond[1], "_")[[1]][3]
-  ) %>%
-  ungroup()
-
-
-out %>%
-  filter(subjectnr == 43, item %in% c(3, 6, 7, 14, 24))
+  out <- dataf %>%
+    filter(
+      !(item %in% c(14, 24)),
+      subjectnr != 4,
+      !(subjectnr == 40 & item == 19),
+      !(subjectnr == 13 & item == 25),
+      !(subjectnr == 43 & (item == 3 | item == 6 | item == 7)),
+      !(subjectnr == 20 & item == 30)
+    ) %>%
+    group_by(cond) %>%
+    mutate(
+      quan = strsplit(cond[1], "_")[[1]][1],
+      subj = strsplit(cond[1], "_")[[1]][2],
+      obj = strsplit(cond[1], "_")[[1]][3]
+    ) %>%
+    ungroup()
 
 
-# remove saccade time from fixation time (for rpdur, tgdur, gdur) and save it again as rpdur, tgdur, gdur
+  out %>%
+    filter(subjectnr == 43, item %in% c(3, 6, 7, 14, 24))
 
-out$rpdur <- out$rpdur - out$rpsacc
-out$tgdur <- out$tgdur - out$tgsacc
-out$gdur <- out$gdur - out$gsacc
 
-out <- out %>% select(-c("rpsacc", "tgsacc", "gsacc"))
+  ## remove saccade time from fixation time
+  ## (for rpdur, tgdur, gdur) and save it again as rpdur, tgdur, gdur
 
-out %>%
-  select(-cond, -expname) %>%
-  rename(
-    subj = subjectnr, subj_cond = subj, obj_cond = obj,
-    quan_cond = quan, region = code
-  ) %>%
-  write_csv(here("results/allACTFiles_clean_sacccadtetime_removed.csv"))
+  out$rpdur <- out$rpdur - out$rpsacc
+  out$tgdur <- out$tgdur - out$tgsacc
+  out$gdur <- out$gdur - out$gsacc
+
+  out <- out %>% select(-c("rpsacc", "tgsacc", "gsacc"))
+
+  out %>%
+    select(-cond, -expname) %>%
+    rename(
+      subj = subjectnr, subj_cond = subj, obj_cond = obj,
+      quan_cond = quan, region = code
+    ) %>%
+    write_csv(here("results/allACTFiles_clean_sacccadtetime_removed.csv"))
+}
+
+main <- function(){
+  prepare_data("data/allACTFiles_vp.csv")
+}
+
+if (sys.nframe() == 0) {
+  main()
+}
--- src/02-regions.R
+++ src/02-regions.R
@@ -9,44 +9,60 @@ library(readr)
 library(purrr)
 library(dplyr)
 
+make_regions <- function(data_in) {
+  dataf <- data_in %>%
+    mutate(
+      cond = case_when(
+        quan_cond == "EEN" & subj_cond == "MATCH" & obj_cond == "MATCH" ~ "a",
+        quan_cond == "EEN" & subj_cond == "MATCH" & obj_cond == "MIS" ~ "b",
+        quan_cond == "EEN" & subj_cond == "MIS" & obj_cond == "MATCH" ~ "c",
+        quan_cond == "EEN" & subj_cond == "MIS" & obj_cond == "MIS" ~ "d",
+        quan_cond == "GEEN" & subj_cond == "MATCH" & obj_cond == "MATCH" ~ "e",
+        quan_cond == "GEEN" & subj_cond == "MATCH" & obj_cond == "MIS" ~ "f",
+        quan_cond == "GEEN" & subj_cond == "MIS" & obj_cond == "MATCH" ~ "g",
+        quan_cond == "GEEN" & subj_cond == "MIS" & obj_cond == "MIS" ~ "h"
+      ),
+      quants = ifelse(quan_cond == "EEN", -1, 1),
+      typic = ifelse(cond %in% c("a", "b", "e", "f"), 1, -1), # subj
+      interf = ifelse(cond %in% c("a", "c", "e", "g"), 1, -1), # obj
+      typic_cond = ifelse(typic == 1, "typical", "atypical"),
+      interf_cond = ifelse(interf == 1, "interf", "no_interf"),
+      ## additional variables
+      ## rrdur = totfixdur - (gdur - gsacc), # re-reading duration
+      ## corrected re-reading duration;
+      ## we don't subtract gsacc because that was already done
+      rrdur = totfixdur - gdur,
+      rr = as.numeric(rrdur > 0)
+    )
 
-dataf <- read_csv(here("results/allACTFiles_clean_sacccadtetime_removed.csv")) %>%
-  mutate(
-    cond = case_when(
-      quan_cond == "EEN" & subj_cond == "MATCH" & obj_cond == "MATCH" ~ "a",
-      quan_cond == "EEN" & subj_cond == "MATCH" & obj_cond == "MIS" ~ "b",
-      quan_cond == "EEN" & subj_cond == "MIS" & obj_cond == "MATCH" ~ "c",
-      quan_cond == "EEN" & subj_cond == "MIS" & obj_cond == "MIS" ~ "d",
-      quan_cond == "GEEN" & subj_cond == "MATCH" & obj_cond == "MATCH" ~ "e",
-      quan_cond == "GEEN" & subj_cond == "MATCH" & obj_cond == "MIS" ~ "f",
-      quan_cond == "GEEN" & subj_cond == "MIS" & obj_cond == "MATCH" ~ "g",
-      quan_cond == "GEEN" & subj_cond == "MIS" & obj_cond == "MIS" ~ "h"
-    ),
-    quants = ifelse(quan_cond == "EEN", -1, 1),
-    typic = ifelse(cond %in% c("a", "b", "e", "f"), 1, -1), # subj
-    interf = ifelse(cond %in% c("a", "c", "e", "g"), 1, -1),# obj
-    typic_cond = ifelse(typic == 1, "typical", "atypical"),
-    interf_cond = ifelse(interf == 1, "interf", "no_interf"),
-    ## additional variables
-    ## rrdur = totfixdur - (gdur - gsacc), # re-reading duration
-    ## corrected re-reading duration;
-    ## we don't subtract gsacc because that was already done
-    rrdur = totfixdur - gdur,
-    rr = as.numeric(rrdur > 0)
+
+  vars <- c(
+    "subj", "item", "rpdur", "tgdur", "totfixdur", "gbck",
+    "gdur", "rr", "rrdur"
   )
 
+  dataf %>%
+    select(
+      region,
+      all_of(vars),
+      quan_cond,
+      typic_cond,
+      interf_cond,
+      cond:interf
+    ) %>%
+    split(.$region)
+}
 
-vars <- c("subj", "item", "rpdur", "tgdur", "totfixdur", "gbck",
-          "gdur", "rr", "rrdur")
-
-dataf %>%
-  select(
-    region,
-    all_of(vars),
-    quan_cond,
-    typic_cond,
-    interf_cond,
-    cond:interf
+main <- function() {
+  make_regions(
+    read_csv(here("results/allACTFiles_clean_sacccadtetime_removed.csv"))
   ) %>%
-  split(.$region) %>%
-  walk(~ write_csv(.x, here("results", paste0("region", .$region[1], ".csv"))))
+    walk(~ write_csv(
+      .x,
+      here("results", paste0("region", .$region[1], ".csv"))
+    ))
+}
+
+if (sys.nframe() == 0) {
+  main()
+}
