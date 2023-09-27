b99985708a363fa23aaab65bc97246df1cf14eb5
jas <jaswinko@protonmail.com>
2023-09-27 15:52:42 +0200

    move to archive

--- src/c_toolchain_config.R
+++
@@ -1,8 +0,0 @@
-dotR <- file.path(Sys.getenv("HOME"), ".R")
-if (!file.exists(dotR)) dir.create(dotR)
-M <- file.path(dotR, "Makevars")
-if (!file.exists(M)) file.create(M)
-cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
-  "CXX14=g++", # or clang++ but you may need a version postfix
-  file = M, sep = "\n", append = TRUE
-)
