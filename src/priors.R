a371ee9bbbe4714547a8dc0ec3c8307f7c7eacf1
jas <jaswinko@protonmail.com>
2023-10-06 17:24:31 +0200

    remove unused priors

--- src/priors.R
+++ src/priors.R
@@ -13,52 +13,9 @@ priors <- c(
   prior(lkj(2), class = cor)
 )
 
-priors_narrow_int <- c(
-  prior(normal(5, 5), class = Intercept),
-  prior(normal(0, 1), class = b),
-  prior(normal(0, 1), class = sigma),
-  prior(normal(0, 1), class = sd),
-  prior(lkj(2), class = cor)
-)
-
-priors_ni_big_sd <- c(
-  prior(normal(5, 5), class = Intercept),
-  prior(normal(0, 1), class = b),
-  prior(normal(0, 1), class = sigma),
-  prior(normal(0, 3), class = sd),
-  prior(lkj(2), class = cor)
-)
-
-priors_ni_big_sd_sigma <- c(
-  prior(normal(5, 5), class = Intercept),
-  prior(normal(0, 1), class = b),
-  prior(normal(0, 3), class = sigma),
-  prior(normal(0, 3), class = sd),
-  prior(lkj(2), class = cor)
-)
-
 priors_binom <- c(
   prior(normal(0, 1.5), class = Intercept),
   prior(normal(0, 1), class = b),
   prior(normal(0, 1), class = sd),
   prior(lkj(2), class = cor)
-)
-
-priors_gamma <- c(
-  prior(normal(0, 10), class = Intercept),
-  prior(normal(0, 1), class = b),
-  prior(normal(0, 1), class = sd),
-  prior(lkj(2), class = cor)
-)
-
-priors_mix <- c(
-  prior(normal(5, 5), class = Intercept, dpar = mu1),
-  prior(normal(6, 4), class = Intercept, dpar = mu2),
-  prior(normal(0, 1), class = b, dpar = mu1),
-  prior(normal(0, 1), class = b, dpar = mu2),
-  prior(normal(0, 1), class = sigma1),
-  prior(normal(0, 1), class = sigma2),
-  prior(normal(0, 1), class = sd, dpar = mu1),
-  prior(normal(0, 1), class = sd, dpar = mu2)
-  ## prior(normal(0, 1), class = theta)
 )
